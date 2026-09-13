5df3cf1e1fd31ec4cd7525aaaaab6ea1651d7e96
MercaTax <mercataxpr@gmail.com>
2026-09-13 02:57:00 -0400

    chore(cleanup): remove legacy icon-preview service worker version

--- pwa-register.js
+++ pwa-register.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js?v=icon-preview-r3', { scope: '/' })
+    navigator.serviceWorker.register('/sw.js', { scope: '/' })
       .catch(err => console.error('MercaTax PWA service worker registration failed', err));
   });
 }
